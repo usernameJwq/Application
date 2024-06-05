0461b227931408fe055444a26daaf242769fab57
Jiangwq <1687145462@qq.com>
2024-06-05 22:14:51 +0800

    feat: image texture render

--- apps/Application.cpp
+++ apps/Application.cpp
@@ -2,6 +2,7 @@
 
 #include "../base/log.h"
 #include "../opengl/glfw_base.h"
+#include "../opengl/image_texture.h"
 
 #define APP_VERSION "1.0.0"
 
@@ -9,8 +10,11 @@ int main() {
     init_logger();
     LOG::info("APP VERSION {}", APP_VERSION);
 
-    GlfwBase glfw_base;
-    glfw_base.show_window();
+    // GlfwBase glfw_base;
+    // glfw_base.show_window();
+
+    ImageTexture img_texture;
+    img_texture.show_window();
 
     return 0;
 }
