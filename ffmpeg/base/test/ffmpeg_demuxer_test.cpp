d456780f642679fdf553925cc8ac4df198e57f6b
Jiangwq <1687145462@qq.com>
2024-07-13 21:31:23 +0800

    feat: rename ffmpeg codec to demuxer

---
+++ ffmpeg/base/test/ffmpeg_demuxer_test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "gtest/gtest.h"
+
+#include "../ffmpeg_demuxer.h"
+
+class FFmpegDemuxerTest : public ::testing::Test {
+protected:
+    void SetUp() override { ffmpeg_demuxer_ = std::make_unique<FFmpegDemuxer>(); }
+
+    std::unique_ptr<FFmpegDemuxer> ffmpeg_demuxer_;
+};
+
+TEST_F(FFmpegDemuxerTest, file_demuxer) {
+    std::string filename = "F:/Qt_Cpp/cmake_pro/Application/assets/video/road.mp4";
+    ffmpeg_demuxer_->file_demuxer(filename);
+}
