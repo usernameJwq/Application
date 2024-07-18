689035ad456e9bd57435ca3aa8bf18a69e7eb9e2
Jiangwq <1687145462@qq.com>
2024-07-18 20:02:56 +0800

    feat: add ffmpeg pro module

---
+++ ffmpeg/pro/rtaduio_pro/rtaudio_player.h
@@ -0,0 +1,12 @@
+#ifndef RTAUDIO_PLAYER_
+#define RTAUDIO_PLAYER_
+
+class RtAudioPlayer {
+public:
+    RtAudioPlayer();
+    ~RtAudioPlayer();
+
+private:
+};
+
+#endif // !RTAUDIO_PLAYER_
---
+++ ffmpeg/pro/rtaduio_pro/test/rtaudio_player_test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "gtest/gtest.h"
+
+class RtAudioPlayerTest : public ::testing::Test {
+public:
+    void SetUp() override {}
+};
+
+TEST_F(RtAudioPlayerTest, test) {}
