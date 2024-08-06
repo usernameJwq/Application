113b120e2ad695362d7f307a6e920f123c7fbf47
Jiangwq <1687145462@qq.com>
2024-08-06 20:39:53 +0800

    feat: add video filter

--- ffmpeg/base/ffmpeg_filter.h
+++ ffmpeg/base/ffmpeg_filter.h
@@ -7,6 +7,10 @@ extern "C" {
 #include "libavformat/avformat.h"
 #include "libavcodec/avcodec.h"
 #include "libavfilter/avfilter.h"
+#include "libavfilter/buffersrc.h"
+#include "libavfilter/buffersink.h"
+#include "libswscale/swscale.h"
+#include "libavutil/imgutils.h"
 #include "libavutil/avutil.h"
 #include "libavutil/opt.h"
 }
@@ -17,19 +21,39 @@ public:
     ~FFmpegFilter();
 
 public:
-    void open_filter_file(const std::string& filename, const std::string& filter_desc);
-    void add_video_filter(const AVCodecContext* video_context, const AVStream* video_stream,
+    void add_video_filter(const std::string& input_filename, const std::string& output_filename,
                           const std::string& filter_desc);
 
 private:
+    void open_input_file(const std::string& filename);
+    void init_encoder();
+    void init_filter(const std::string& filter_desc);
+    void init_out_context(const std::string& output_filename);
+
+private:
+    void decodec();
+    void write_video_buffer(AVPacket* packet);
+    void video_frame_src_to_sink(AVFrame* frame, AVPacket* src_packet);
+    void recoder_video(AVFrame* frame, AVPacket* src_packet);
+
+private:
+    AVFrame* video_scale(AVFrame* frame);
+
+private:
     AVFormatContext* avformat_context_;
+    AVFormatContext* avformat_out_context_;
+
     AVCodecContext* video_context_;
+    AVCodecContext* audio_context_;
+    AVCodecContext* video_encoder_context_;
+    AVCodecContext* audio_encoder_context_;
+
+    AVStream* video_stream_;
+    AVStream* audio_stream_;
 
     AVFilterContext* buffer_context_;
     AVFilterContext* buffersink_context_;
     AVFilterGraph* filter_graph_;
-
-    int video_index_;
 };
 
 #endif // !FFMPEG_FILTER_
